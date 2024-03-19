f8cf819a341ee8d24eb661d5ad2c5b1e14ba7bbc
bagasadikurniawan <21500022.bagas@sinus.ac.id>
2024-03-19 20:23:39 +0700

    Merubah pseudocode.js

--- pseudocode.js
+++ pseudocode.js
@@ -1,24 +1,51 @@
 // Algortima Pseudocode
 
 /** 
-* 1. Algoritma dan Pseudocode keliling Persegi Panjang 
-     - Tentukan Panjang Persegi Panjang
-     - Tentukan Lebar Persegi Panjang
-     - Tentukan Rumus Keliling
-     - Hitung K = (P+L)*2
+* 1. Algoritma  keliling Persegi Panjang 
+     - Buat variabel panjang
+     - Buat Variabel lebar
+     - Buat Variabel Keliling
+     - Masukan rumus Keliling = 2 x (Panjang+Lebar)
+     - cetak Keliling
+
+     Pseudocode Persego Panjang
+     - STORE Panjang WITH ANY NUMBER
+     - STORE Lebar WITH ANY NUMBER
+     
+     - STORE Kelilling WITH 2 MUL Panjang ADD 2 MUL Lebar
+     - DISPLAY Keliling
+
+
+  2. ALgoritma Luas Permukaan Tabung
+     - Cari Luas Permukaan Tabung = 2 x pi x r x r + 2 x p1 x r x t
+     - Buat Variabel pi
+     - Buat Variabel r
+     - Buat Variabel t
+     - Buat Variable LuasPermukaan
+     - Isi LuasPermukaan dengan rumus = 2 x pi x r x r + 2 x p1 x r x t
+     - Cetak LuasPermukaan
+
+     Pseudocode Luas Permukaan Tabung
+     - STORE pi WITH 3.14
+     - STORE r WITH ANY NUMBER
+     - STORE t WITH ANY NUMBER
+
+     -STORE LuasPermukaan WITH 2 MUL pi MUL r MUL r ADD 2 MUL p1 MUL r MUL t
+
+
+   3. Algoritma  Volume Bola
+     - Cari Rumus Volume Bola = 4/3 x pi x r x r x r
+     - Buat Variabel pi
+     - Buat Variabel r
+     - Buat Variabel Volume
+     - Isi Volume dengan rumus = 1/4 x 4/3 x pi x r x r x r
+     - Cetak Volume
+
+    Pseudocode Volume Bola
+     - STORE pi WITH 3.14
+     - STORE r WITH ANY NUMBER
+
+     - STORE Volume WITH 1/4 MUL 4/3 MUL pi MUL r MUL r MUL r
+     - DISPLAY Volume
+
 */
-
-    // Mendefinisikan fungsi untuk menghitung keliling persegi panjang
-    function hitungKelilingPersegiPanjang(panjang, lebar) {
-        return 2 * (panjang + lebar);
-    }
-
-    // Meminta pengguna untuk memasukkan panjang dan lebar persegi panjang
-    let panjang = parseFloat(prompt("Masukkan panjang persegi panjang:"));
-    let lebar = parseFloat(prompt("Masukkan lebar persegi panjang:"));
-
-    // Menghitung keliling persegi panjang
-    let keliling = hitungKelilingPersegiPanjang(panjang, lebar);
-
-    // Menampilkan hasil keliling
-    console.log("Keliling persegi panjang adalah: " + keliling);
